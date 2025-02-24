81005f5310a0b588fa98ab00d458665bffe62ec3
33abdal33 <abdallizanomuriel08@gmail.com>
2025-02-24 05:43:50 -0500

    Add new Repository

---
+++ Sistema De Ventas/Repository/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Sistema_De_Ventas.Models;
+
+namespace Sistema_De_Ventas.Repository
+{
+    public interface IRepositoryBase<TEntity> where TEntity : EntityBase
+    {
+        Task<ICollection<TEntity>> GetAsync();
+        Task<TEntity> GetAsync(int id);
+        Task<int> AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+
+    }
+}
---
+++ Sistema De Ventas/Repository/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema_De_Ventas.Models;
+
+namespace Sistema_De_Ventas.Repository
+{
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : EntityBase
+    {
+        protected readonly DbContext context;
+        public RepositoryBase(DbContext context)
+        {
+            this.context = context;
+        }
+        public virtual async Task<int> AddAsync(TEntity entity)
+        {
+            await context.Set<TEntity>().AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity.Id;
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<TEntity>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
