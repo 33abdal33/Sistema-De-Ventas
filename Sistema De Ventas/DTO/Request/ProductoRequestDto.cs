d175c118dca1956283a645f81f1494c5785e9547
33abdal33 <abdallizanomuriel08@gmail.com>
2025-02-25 03:33:21 -0500

    Create DTO - request and response

---
+++ Sistema De Ventas/DTO/Request/ProductoRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Sistema_De_Ventas.DTO.Request
+{
+    public class ProductoRequestDto
+    {
+        public string nombre { get; set; } = default!;
+        public string descripcion { get; set; } = default!;
+        public decimal precio { get; set; }
+        public int stock { get; set; }
+    }
+}
---
+++ Sistema De Ventas/DTO/Response/ProductoResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Sistema_De_Ventas.DTO.Response
+{
+    public class ProductoResponseDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = default!;
+        public string Descripcion { get; set; } = default!;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
