3d4e87035d0ea92206fcd2f7afec3ba23db21586
33abdal33 <abdallizanomuriel08@gmail.com>
2025-02-25 03:35:03 -0500

    Implementation Services

---
+++ Sistema De Ventas/Services/Implementation/ProductoServices.cs
@@ -0,0 +1,33 @@
+﻿using Sistema_De_Ventas.DTO.Request;
+using Sistema_De_Ventas.DTO.Response;
+using Sistema_De_Ventas.Services.Interfaces;
+
+namespace Sistema_De_Ventas.Services.Implementation
+{
+    public class ProductoServices : IProductoServices
+    {
+        public ProductoServices( )
+        {
+            
+        }
+        public Task<BaseResponseGeneric<int>> AddAsync(ProductoRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponseGeneric<ProductoResponseDto>> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse> UpdateAsync(int id, ProductoRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sistema De Ventas/Services/Interfaces/IProductoServices.cs
@@ -0,0 +1,13 @@
+﻿using Sistema_De_Ventas.DTO.Request;
+using Sistema_De_Ventas.DTO.Response;
+
+namespace Sistema_De_Ventas.Services.Interfaces
+{
+    public interface IProductoServices
+    {
+        Task<BaseResponseGeneric<ProductoResponseDto>> GetAsync(int id);
+        Task<BaseResponseGeneric<int>> AddAsync(ProductoRequestDto request);
+        Task<BaseResponse> UpdateAsync(int id, ProductoRequestDto request);
+        Task<BaseResponse> DeleteAsync(int id);
+    }
+}
